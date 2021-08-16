23b3547dd57e5fcd9d9cee7f1d29e3b6ef391cdc
SanShamor <sanshamor@mail.ru>
2021-08-16 21:47:56 +0300

    Initial Commit

---
+++ StateAndDataFlowApp/StateAndDataFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowAppApp.swift
+//  StateAndDataFlowApp
+//
+//  Created by Александр on 16.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
